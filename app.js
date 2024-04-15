68e0ecacc9c4fc0306e38a87e19263c312fa7f2b
MuammadAnusAhmed <MUHAMMADANUSAHMEDON@gmail.com>
2024-04-15 18:17:09 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Addition
+var additionResult = 5 + 3;
+console.log("Addition: = ".concat(additionResult));
+// Subtraction
+var subtractionResult = 10 - 2;
+console.log("Subtraction: = ".concat(subtractionResult));
+// Multiplication
+var multiplicationResult = 4 * 2;
+console.log("Multiplication: = ".concat(multiplicationResult));
+// Division
+var divisionResult = 16 / 2;
+console.log("Division: = ".concat(divisionResult));
